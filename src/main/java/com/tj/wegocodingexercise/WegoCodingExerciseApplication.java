eb2818b3d2d71d92816f90514c8078b042a7496c
tjlerias <25985002+tjlerias@users.noreply.github.com>
2023-10-17 22:05:04 +0800

    Project set up

---
+++ src/main/java/com/tj/wegocodingexercise/WegoCodingExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.tj.wegocodingexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WegoCodingExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WegoCodingExerciseApplication.class, args);
+	}
+
+}
