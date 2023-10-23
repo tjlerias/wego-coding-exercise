b60df8053cdc5e4e64305145699622f904757d5c
tjlerias <25985002+tjlerias@users.noreply.github.com>
2023-10-24 01:14:29 +0800

    Add json property order

--- src/main/java/com/tj/wegocodingexercise/dto/CarParkDetails.java
+++ src/main/java/com/tj/wegocodingexercise/dto/CarParkDetails.java
@@ -1,7 +1,9 @@
 package com.tj.wegocodingexercise.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+@JsonPropertyOrder({"address", "latitude", "longitude", "total_lots", "available_lots"})
 public record CarParkDetails(
     String address,
     Double latitude,
